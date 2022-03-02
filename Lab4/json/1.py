3c5d25f0da92df8c0da351060f0a87c716cbe82d
SamSweet04 <98650996+SamSweet04@users.noreply.github.com>
2022-03-03 02:45:06 +0600

    Add files via upload

--- Lab4/json/1.py
+++ Lab4/json/1.py
@@ -6,14 +6,14 @@ d = json.load(f)
 print('Interface Status','='* 90,sep='\n')
 print('DN' , ' '*60 , 'Description' , ' '*6, 'Speed' , ' '*4 , 'MTU') 
 print('-'*61 , ' ' , "-" *15 , " "*2 , "-"*6 , " "*3 , "-"*6)
-y=0
+cnt = 0
 for i in d['imdata']:
-    y+=1
+    cnt += 1
     out1=i["l1PhysIf"]["attributes"]["dn"]
     out11=i["l1PhysIf"]["attributes"]["fecMode"]
     out111=i["l1PhysIf"]["attributes"]["mtu"]
 
     print(out1,' '*39, out11,' ' * 2,out111)
-    if y == 3:
-        break
+    if cnt == 3:
+        exit()
 
