1b4934c69cdcbec7ed476693dac578bb598de2c2
SamSweet04 <98650996+SamSweet04@users.noreply.github.com>
2022-03-17 11:58:16 +0600

    Add files via upload

--- Lab6/dir and file/6.py
+++ Lab6/dir and file/6.py
@@ -1,6 +1,9 @@
-import os,string
-if not os.path.exists("file"):
-    os.makedirs("file")
-for i in string.ascii_uppercase:
-    with open(i + ".txt","w") as f1:
-        f1.writelines(i)
+# import os,string
+# if not os.path.exists("file"):
+#     os.makedirs("file")
+# for i in string.ascii_uppercase:
+#     with open(i + ".txt","w") as f1:
+#         f1.writelines(i)
+
+for i in range(26):
+    open(chr(i+65) + '.txt', 'w')
